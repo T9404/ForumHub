a3b9aa363bae0c949c93e4650fa42162005cc799
T9404 <ghubman1@gmail.com>
2024-02-29 22:36:20 +0700

    fixed problem with docker environment

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -2,8 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.netty.NettyAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                GsonAutoConfiguration.class,
+                MultipartAutoConfiguration.class,
+                WebSocketServletAutoConfiguration.class,
+                NettyAutoConfiguration.class,
+                DataSourceAutoConfiguration.class
+        }
+)
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
